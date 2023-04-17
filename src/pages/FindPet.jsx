60918b9d9ec1237ec0265f718446f4d907176b31
MatiIL <matibres@gmail.com>
2023-04-17 18:13:57 +0300

    FindPet debug

--- src/pages/FindPet.jsx
+++ src/pages/FindPet.jsx
@@ -1,6 +1,7 @@
 import SearchForm from '../components/SearchForm'
 
 function FindPet() {
+    console.log("this is the pet search page")
     return (
         <div className="search-container ms-3">
             <SearchForm/>
